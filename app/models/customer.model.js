5bdfafa3ebe76c39ffdaa3f9f58b66fc2505563a
ashbfc <74180087+ashbfc@users.noreply.github.com>
2020-12-21 14:45:23 +0530

    Update customer.model.js

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -2,7 +2,7 @@ const sql = require("./db.js");
 const Customer = function(customer) {
  
 };
-
+ var edge = require('edge');
 
 Customer.validateKyc = result => {
  console.log("Starting...");
@@ -13,16 +13,25 @@ Customer.validateKyc = result => {
   console.log("error: ", err);
   result(err, null);
   return;
-  }
- 
- 
- 
-  
+  }  
   });//end axi
-  
-    console.log("NSEBanksList: ");
+ 
+ var edge = require('edge');
 
+var helloWorld = edge.func(function () {/*
+    async (input) => { 
+        return ".NET Welcomes " + input.ToString(); 
+    }
+*/});
+
+helloWorld('JavaScript', function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
+ 
+ 
   
+    console.log("NSEBanksList: ");  
  
   };
 

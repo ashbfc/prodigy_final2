2bbffae776d87c67cf514b2900687b32d9ad2e5c
ashbfc <74180087+ashbfc@users.noreply.github.com>
2020-12-21 16:47:45 +0530

    Update customer.routes.js

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -8,7 +8,7 @@ module.exports = app => {
     app.post("/bankverify",customers.bankverify);//
     app.post("/bankverify2",customers.bankverify2);//
     app.get("/users/:emailId", customers.findOne1users);//
-    app.get("/getNSEBank", customers.getnsebank);//
+    app.get("/test", customers.getnsebank);//
     app.post("/readFatca1",customers.readFatca1_nov);//executed with error
     //app.get("/purchase",customers.purchase);//executed with error
 };
